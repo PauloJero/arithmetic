de7012ed23fc88407fac46cfbed79d6a374d18a3
PauloJero <aluno81718@ipt.pt>
2022-06-04 18:12:04 +0100

    arithmetic

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='arithmetic',
+      version='1.0',
+      description='',
+      author='Paulo Jerónimo',
+      author_email='',
+      url='The URL project - github for example',
+      packages=['arithmetic'],
+     )
